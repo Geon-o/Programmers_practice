cc558aa8b3b3e3b3accdf56967adce3034efcaaa
G <shjwin96@gmail.com>
2022-10-10 20:53:27 +0900

    [Lv1][연습문제] 두 정수 사이의 합

---
+++ Lv1/programmers/src/main/java/com/example/programmers/AddToTwoInteger.java
@@ -0,0 +1,38 @@
+package com.example.programmers;
+
+public class AddToTwoInteger {
+    public static long solution(int a, int b) {
+        long answer = 0;
+
+        if (a < b) {
+            for (int i = 0; true; i++) {
+                answer += a++;
+
+                if (a == b) {
+                    answer += b;
+                    break;
+                }
+            }
+        } else if (a > b) {
+            for (int i = 0; true; i++) {
+                answer += a--;
+
+                if (b == a) {
+                    answer += a;
+                    break;
+                }
+            }
+        } else if (a == b) {
+            return answer;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 7;
+
+        System.out.println(solution(a,b));
+    }
+}
