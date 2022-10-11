0875ace4049fcf83cc95d016ebc94c37caac85de
G <shjwin96@gmail.com>
2022-10-11 15:14:55 +0900

    [Lv1][연습문제] 나누어 떨어지는 숫자 배열

---
+++ Lv1/programmers/src/main/java/com/example/programmers/DivisionNumArr.java
@@ -0,0 +1,35 @@
+package com.example.programmers;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class DivisionNumArr {
+
+    public static List<Integer> solution(int[] arr, int divisor) {
+        List<Integer> answer = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                answer.add(arr[i]);
+                count++;
+            }
+        }
+        answer.sort(Comparator.naturalOrder());
+
+        if (count == 0) {
+            answer.add(-1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+
+        System.out.println(solution(arr, divisor));
+
+    }
+}
