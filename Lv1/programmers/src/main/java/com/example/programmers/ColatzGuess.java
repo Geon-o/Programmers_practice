050c483c463ad11350f612ceff6a5a504d135755
G <shjwin96@gmail.com>
2022-10-13 13:55:40 +0900

    [Lv1][연습문제] 콜라츠 추측

---
+++ Lv1/programmers/src/main/java/com/example/programmers/ColatzGuess.java
@@ -0,0 +1,30 @@
+package com.example.programmers;
+
+public class ColatzGuess {
+    public static void main(String[] args) {
+        long n = 16;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(long num) {
+        int answer = 0;
+        long currentNum = num;
+
+        while (currentNum != 1) {
+
+            if (currentNum % 2 == 0) {
+                currentNum /= 2;
+                answer++;
+
+            } else if (currentNum % 2 != 0) {
+                currentNum = currentNum * 3 + 1;
+                answer++;
+            }
+
+            if (answer == 500) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
