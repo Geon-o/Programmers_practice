579b4a935e8e39162c62bc86663c709ae80d6580
G <shjwin96@gmail.com>
2022-10-13 13:58:08 +0900

    [Lv1][연습문제] 하샤드 수

---
+++ Lv1/programmers/src/main/java/com/example/programmers/HarshadNumber.java
@@ -0,0 +1,25 @@
+package com.example.programmers;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println(solution(x));
+    }
+
+    public static boolean solution(int x) {
+        boolean answer = true;
+        int num = x;
+        int sum = 0;
+
+        while(num >= 1){
+            sum += num % 10;
+            num /= 10;
+        }
+        if(x % sum ==0){
+            answer = true;
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+}
