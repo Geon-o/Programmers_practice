b3256f5bcc8921cacfd910dcc8cd947160f2d445
G <shjwin96@gmail.com>
2022-10-13 13:30:01 +0900

    [Lv1][연습문제] 없는 숫자 더하기

---
+++ Lv1/programmers/src/main/java/com/example/programmers/AddMissingNumber.java
@@ -0,0 +1,35 @@
+package com.example.programmers;
+
+public class AddMissingNumber {
+
+    public static int solution(int[] numbers) {
+        int answer = 0;
+        int currentAddNum = 0;
+        int addOneToNine = addNumToNum();
+
+        for (int i = 0; i < numbers.length; i++) {
+            currentAddNum += numbers[i];
+        }
+        answer = addOneToNine - currentAddNum;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {5,8,4,0,6,7,9};
+
+        System.out.println(solution(numbers));
+    }
+
+    public static int addNumToNum() {
+        int addNum = 0;
+        int startNum = 1;
+        int endNum = 9;
+
+        for (int i = 0; i < endNum; i++) {
+            addNum += startNum;
+            startNum++;
+        }
+        return addNum;
+    }
+}
