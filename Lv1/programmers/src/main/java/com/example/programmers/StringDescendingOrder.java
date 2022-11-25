a4ea0b274657d48c825ec6d0a84de5f5022ee10a
G <shjwin96@gmail.com>
2022-11-25 16:03:01 +0900

    [Lv1][연습문제] 문자열 내림차순으로 배치하기

---
+++ Lv1/programmers/src/main/java/com/example/programmers/StringDescendingOrder.java
@@ -0,0 +1,22 @@
+package com.example.programmers;
+
+
+import java.util.Arrays;
+
+public class StringDescendingOrder {
+    public static void main(String[] args) {
+        String s = "Zbcdefg";
+
+        System.out.println(solution(s));
+    }
+
+    public static String solution(String s) {
+        String answer = "";
+
+        char[] charArr = s.toCharArray();
+        Arrays.sort(charArr);
+
+        answer = new StringBuilder(new String(charArr)).reverse().toString();
+        return answer;
+    }
+}
