9e6a07ec263ef6f55adce8869a82844408e35902
G <shjwin96@gmail.com>
2022-11-27 20:44:28 +0900

    [Lv1][연습문제] 문자열 다루기 기본

---
+++ Lv1/programmers/src/main/java/com/example/programmers/StringConsistingOnlyOfNumbers.java
@@ -0,0 +1,34 @@
+package com.example.programmers;
+
+public class StringConsistingOnlyOfNumbers {
+
+    public static void main(String[] args) {
+        String s = "09786A";
+
+        System.out.println(solution(s));
+    }
+
+    public static Boolean solution(String s) {
+        boolean answer = true;
+
+        boolean stringLen = (s.length() == 4 || s.length() == 6);
+
+        if (stringLen) {
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+
+                if (ch >= '0' && ch <= '9') {
+                    answer = true;
+                }
+                else {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        else {
+            answer = false;
+        }
+        return answer;
+    }
+}
