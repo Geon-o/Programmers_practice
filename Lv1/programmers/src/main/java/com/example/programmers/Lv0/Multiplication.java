e29adf68b22780698b4e6234d2b92bc4ef178c80
G <shjwin96@gmail.com>
2023-02-07 18:54:54 +0900

    [Lv0][연습문제] 두 수의 곱

---
+++ Lv1/programmers/src/main/java/com/example/programmers/Lv0/Multiplication.java
@@ -0,0 +1,18 @@
+package com.example.programmers.Lv0;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+
+        answer = num1 * num2;
+
+        return answer;
+    }
+}
