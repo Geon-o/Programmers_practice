d53b8134b5f56ace43d18eee202f111dc250cc47
G <shjwin96@gmail.com>
2023-02-07 22:30:23 +0900

    [Lv0][연습문제] 분수의 덧셈

--- Lv1/programmers/src/main/java/com/example/programmers/Lv0/Division.java
+++ Lv1/programmers/src/main/java/com/example/programmers/Lv0/Division.java
@@ -1,20 +1,37 @@
 package com.example.programmers.Lv0;
 
+import java.util.Arrays;
+
 public class Division {
     public static void main(String[] args) {
-        int num1 = 3;
-        int num2 = 2;
+        int numer1 = 9;
+        int denom1 = 2;
+        int numer2 = 1;
+        int denom2 = 3;
 
-        System.out.println(solution(num1, num2));
+        System.out.println(Arrays.toString(solution(numer1, denom1, numer2, denom2)));
     }
 
-    public static int solution(float num1, float num2) {
-        int answer = 0;
-        float divisionNum;
+    public static int[] solution(int numer1,int denom1, int numer2, int denom2) {
+        //최대 공약수를 구해야함
+        int[] answer = new int[2];
 
-        divisionNum = (num1 / num2);
-        answer = (int) (divisionNum * 1000);
+        int numer = (denom1 * numer2) + (denom2 * numer1);
+        int denom = denom1 * denom2;
+        int max = gcd(numer, denom);
+
+        answer[0] = numer / max;
+        answer[1] = denom / max;
 
         return answer;
+    }
+
+    public static int gcd(int numer, int denom) {
+        //최대공약수 로직
+        if (numer % denom == 0) {
+            return denom;
+        }
+        //재귀함수
+        return gcd(denom, numer % denom);
     }
 }
