0ac3ce8a99be65a8e8a92c1feddb0aabda9558a9
G <shjwin96@gmail.com>
2023-02-07 18:52:56 +0900

    [Lv0][연습문제] 두 수의 합

---
+++ Lv1/programmers/src/main/java/com/example/programmers/Lv0/SumTotal.java
@@ -0,0 +1,18 @@
+package com.example.programmers.Lv0;
+
+public class SumTotal {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = -1;
+
+        answer = num1 + num2;
+
+        return answer;
+    }
+}
